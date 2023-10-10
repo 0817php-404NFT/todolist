cd4ff2da651e608936a53fedb19eb01a30dca0a4
catgoesmeowo <mmj0114@naver.com>
2023-10-11 01:58:06 +0900

    detail...

--- src/detail.php
+++ src/detail.php
@@ -2,43 +2,43 @@
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/todolist/src/");
 define("FILE_HEADER", ROOT."header.php");
 require_once(ROOT."lib/lib_db.php");
-try{
-	$id = "";
-	$conn = null;
-	// id확인
-	if(isset($_GET["id"])){
-		$id=$_GET["id"]; //id 셋팅
-	} else {
-		throw new Exception("Parameter ERROR : NO id"); //강제 예외 발생
-	}
+// try{
+// 	$id = "";
+// 	$conn = null;
+// 	// id확인
+// 	if(isset($_GET["id"])){
+// 		$id=$_GET["id"]; //id 셋팅
+// 	} else {
+// 		throw new Exception("Parameter ERROR : NO id"); //강제 예외 발생
+// 	}
 
-	if(!my_db_conn($conn)) {
-		throw new Exception("DB Error : PDO Instance");
-	}
+// 	if(!my_db_conn($conn)) {
+// 		throw new Exception("DB Error : PDO Instance");
+// 	}
 
-	// 게시글 데이터 조회
-	$param_id = [
-		"id" => $id
-	];
-	$result=db_select_board_id($conn, $param_id);
+// 	// 게시글 데이터 조회
+// 	$param_id = [
+// 		"id" => $id
+// 	];
+// 	$result=db_select_board_id($conn, $param_id);
 
-	// 게시글 조회 예외 처리
-	if(!$result) {
-		throw new Exception("DB Error : PDO Select_id");
-	} else if(!(count($result) === 1)){
-		throw new Exception("DB Error : PDO Select_id count," .count($resilt));
-	} 
-$item=$result[0];
+// 	// 게시글 조회 예외 처리
+// 	if(!$result) {
+// 		throw new Exception("DB Error : PDO Select_id");
+// 	} else if(!(count($result) === 1)){
+// 		throw new Exception("DB Error : PDO Select_id count," .count($resilt));
+// 	} 
+// $item=$result[0];
 
-} catch (Exception $e) {
-	echo $e->getMessage(); //예외 메세지 출력
-	exit; //처리종료
-} finally {
-	db_destroy_conn($conn);
-}
-	// 게시글 조회 count 에러
+// } catch (Exception $e) {
+// 	echo $e->getMessage(); //예외 메세지 출력
+// 	exit; //처리종료
+// } finally {
+// 	db_destroy_conn($conn);
+// }
+// 	// 게시글 조회 count 에러
 	
-$page=$_GET["page"];	
+// $page=$_GET["page"];	
 ?>
 
 
@@ -70,11 +70,9 @@ $page=$_GET["page"];
         </table>
 
         <div class="detail-button-container">
-            <a href="/todolist/src/update.php/?id=<?php echo $id; ?>">수정</a>
-            <a href="/todolist/src/delete.php/?id=<?php echo $id; ?>">삭제</a>
-        </div>
-
-
+        <a href="/todolist/src/update.php/?id=<?php echo $id; ?>">수정</a>
+        <a href="/todolist/src/delete.php/?id=<?php echo $id; ?>">삭제</a>
     </div>
+</div>
 </body>
 </html>
