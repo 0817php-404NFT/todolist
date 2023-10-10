5e367b0c3737d784a7085ef8cca8e394741f32e8
HongDawww <d939305@naver.com>
2023-10-10 15:05:29 +0900

    1010 lib 함수 추가

--- src/lib/lib_db.php
+++ src/lib/lib_db.php
@@ -283,5 +283,37 @@ function db_update_boards_id(&$conn, &$arr_param) {
         }
 }
 
+//  ---------------------------------------------
+//  함수명 db_delete_boards_id
+//  기능 : 특정 id의 레코드 삭제처리
+//  파라미터 : pdo &$conn
+//			&$arr_param 	
+//  생성일 : 2023-10-10 홍다윗
+// -----------------------------------------------
+
+
+function db_delete_boards_id(&$conn, &$arr_param) {
+	$sql =
+	" UPDATE boards"
+	." SET "
+	." 		del_date = now() "
+	." 		,delete_flg = '1' "
+	." WHERE "
+	." 		id = :id "
+	;
+
+	$arr_ps = [
+		":id" => $arr_param["id"]
+	];
+
+	try {
+		$stmt = $conn->prepare($sql);
+		$result = $stmt->execute($arr_ps);
+		return $result;
+	} catch (Exception $e) {
+		echo $e->getMessage();
+		return false;
+	} 
+}
 
 ?>
