069cddefafbca5af67cb7965bdf73f5c0ed74e02
catgoesmeowo <mmj0114@naver.com>
2023-10-16 17:00:18 +0900

    main_page

---
+++ src/main.php
@@ -0,0 +1,26 @@
+<?php
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/todolist/src/");
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/todolist/src/css/style.css">
+    <title>메인페이지</title>
+</head>
+<body>
+	<a class="main_a" href="/todolist/src/list.php">
+		<div class="main_jpg">
+			<div class="main_msg">아무곳이나 클릭해 주세요</div>
+			<div class="zzz zzz-zzz">Z</div>
+			<div class="zzz zzz-zz">Z</div>
+			<div class="zzz zzz-z">Z</div>
+			<div class="main_div">꿀잠</div>
+			<img class="main_img_moon" src="/todolist/src/img/moon.png">
+		</div>
+		</a>
+    </body>
+
+</html>
