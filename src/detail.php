8cd1f6f2d9542062280e0d1e482a6dbad21af143
catgoesmeowo <mmj0114@naver.com>
2023-10-17 10:41:55 +0900

    detail.

--- src/detail.php
+++ src/detail.php
@@ -1,13 +1,29 @@
 <?php
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/todolist/src/");
+
+
+
+
+//define 함수는 상수를 정의할때 사용하는 값임, 코드는 "ROOT"라는 이름의 상수를 정의하고, 이 상수에 $_SERVER["DOCUMENT_ROOT"]."/todolist/src/"의 값을 할당함
 // ROOT는 상수의 이름이고$_SERVER["DOCUMENT_ROOT"]는 슈퍼 글로벌 변수 중 하나인
 // _SERVER에서 DOCUMENT_ROOT 값을 가져온것임 결과적으로는 ROOT 상수는 웹 서버의 루트 디렉토리에
 //"/todolist/src/"를 추가한 값을 가지게 되는거셈
+
+
 define("FILE_HEADER", ROOT."header.php");
+
+
+
+
 //"FILE_HEADER" 상수는 "ROOT" 상수의 값인 웹 서버 디렉토리에 "header.php"를 추가한 경로임
 //이러한 상수를 사용하면 스크립트 내에서 일관된 경로 또는 파일 이름을 참조할 때 편리하게 사용 가능함
 //예를 들어, "FILE_HEADER" 상수를 사용하면 "header.php"파일을 다른 스크립트에서 사용할 때 항상 동일한 경로로 참조가능
+
+
 require_once(ROOT."lib/lib_db.php");
+
+
+
 //파일을 포함하거나 불러올때 사용되며,PHP스크립트에서 다른 PHP파일을 포함하거나 불러오는데 사용됨 그래서 lib_db파일을 불러온거임
 
 
@@ -19,6 +35,7 @@ try{
 	$conn = null;
     $page = "";
 	$ox ="";
+
 	//변수를 초기화 함, 이유는 정의되지 않은 값으로부터 보호 하기 위해서
 	//메모리에 내용이 이미 입력되어있으면 원치않는 동작을 유발 할 수 있음
 	//쉽게 생각해서 컴퓨터 프로그램에서 데이터를 저장하는 데 사용되는 메모리를 할당할려고 값 설정함
@@ -34,12 +51,15 @@ try{
 	} else {
 		throw new Exception("Parameter ERROR : NO id"); //강제 예외 발생
 	}
+
+
+
 	//"id"라는 이름의 URL 매개변수(파라미터)가 현재 요청에서 존재하는지 확인하는거임
 	//$_GET 은 PHP의 전역 배열로 현재 URL에서 GET METHOD를 통해 전달된 매개변수를 저장함
 	//isset은 매개변수가 존재하는지 확인하며 "id"매개변수가 존재하면 아래 코드를 실행
 	//"id" 매개변수(파라미터)가 존재하지 않는경우 강제 예외가 발생하고 오류 메세지 출력, 혹은 예외처리 코드로 이동함
 	//$id = $_GET["id"]; "id"매개변수 값을 $id라는 변수에 할당한다 (true) 하면 해당 URL에 매개변수 값을 저장함
-	//id는 지금 페이지의 게시글 순번을 나타냄
+	//id는 주로 테이블에서 각 행(레코드)을 고유하게 식별하는 열(칼럼)을 나타냄 고유한 id가 있다는 뜻임
 
 
 
@@ -50,6 +70,10 @@ try{
 	} else {
 		throw new Exception("Parameter ERROR : NO page"); //강제 예외 발생
 	}
+
+
+
+
 	//"page"라는 이름의 URL 매개변수가 존재하는지 확인함
 	//$_GET 은 GET METHOD를 통해 전달된 매개변수(파라미터)를 저장함
 	//isset으로 매개변수 존재 여부를 검사하고
@@ -67,6 +91,11 @@ try{
 	if(!my_db_conn($conn)) {
 		throw new Exception("DB Error : PDO Instance");
 	}
+
+
+
+
+
 	//if 조건이 참인지 아닌지 괄호 안의 조건을 평가함
 	//!my_db_conn($conn)이게 조건이고 함수가 'false'를 반환할때 참임
 	//왜냐하면 ! 기호는 부정을 의미해서 'false'일때 조건이 참이 됨
@@ -93,6 +122,9 @@ try{
 		"id" => $id
 	];
 	$result=db_select_boards_id($conn, $arr_param);
+
+
+
 	//데이터 베이스 쿼리를 실행할 때 사용되는 $arr_parm을 사용해서
 	//특정 게시판 게시물을 조회하기 위한 조건문 적는데 사용됨
 	//$arr_param = ["id" => $id];으로 배열 내부에 "id"키를 사용해서
@@ -122,6 +154,9 @@ try{
 	} else if(!(count($result) === 1)){
 		throw new Exception("DB Error : PDO Select_id count," .count($result));
 	} 
+
+
+	
 //if문은 만약에 데이터 $result 변수를 검사해서 데이터 베이스 조회 결과가
 //false일 경우(데이터베이스 조회 실패시)예외를 출력시킴
 //에러 메세지는"DB Error : PDO Select_id"로 설정됨
@@ -201,7 +236,7 @@ $item=$result[0];
     <div class="detail-container">
 		<!-- 상세 페이지 컨테이너 정의하는 div임 상세 페이지 콘텐츠 감싸기위해 적음 -->
         <table class="detail_table center">     
-			   <!--테이블 요소를 정의함 detail_table이랑 center클래스 갖고있음 상세 페이지 레이아웃이며 클래스를 써서 스타일 적용함 -->
+				<!--테이블 요소를 정의함 detail_table이랑 center클래스 갖고있음 상세 페이지 레이아웃이며 클래스를 써서 스타일 적용함 -->
             <tr>
 				<!-- tr은 테이블 행을 나타냄 각 행의 콘텐츠를 나누기 위해서 적음 -->
                 <td colspan="3" class="detail_head center">
